20d272188da8a42aa5b5a738850e162dc2bcbab7
irfan <muhdirfansyafi@gmail.com>
2024-07-07 23:57:13 +0800

    Add Feat: Updating routes for pagination

--- routes.py
+++ routes.py
@@ -1,10 +1,15 @@
 # routes.py
-from flask import Flask, render_template
+from flask import render_template, request
 from models import RecyclingCenter
-from main import db, app
+from main import app
 
 @app.route('/')
 def index():
-    centers = RecyclingCenter.query.all()
+    page = request.args.get('page', 1, type=int)
+    per_page = 5
+    centers_paginated = RecyclingCenter.query.paginate(page=page, per_page=per_page, error_out=False)
+    total_pages = centers_paginated.pages
+    centers = centers_paginated.items
+
     centers_dicts = [center.as_dict() for center in centers]
-    return render_template('index.html', centers=centers_dicts)
+    return render_template('index.html', centers=centers_dicts, total_pages=total_pages, current_page=page)
